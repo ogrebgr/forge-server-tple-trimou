5b047f6b766da8e18aa34b4e5040c72ee6e5507f
Ognyan Bankov <ogibankov@gmail.com>
2017-02-05 17:21:51 +0200

    now depending on forge-server 4.0.0+

--- src/main/java/com/bolyartech/forge/server/tple/trimou/TrimouTemplateEngineFactory.java
+++ src/main/java/com/bolyartech/forge/server/tple/trimou/TrimouTemplateEngineFactory.java
@@ -6,12 +6,18 @@ import org.trimou.engine.MustacheEngine;
 import org.trimou.engine.MustacheEngineBuilder;
 import org.trimou.engine.locator.ClassPathTemplateLocator;
 
+import java.io.File;
+
 
 public class TrimouTemplateEngineFactory implements TemplateEngineFactory {
     private final MustacheEngine mEngine;
 
 
     public TrimouTemplateEngineFactory(String templatePathPrefix) {
+        if (templatePathPrefix.startsWith(File.separator)) {
+            templatePathPrefix = templatePathPrefix.substring(1);
+        }
+
         mEngine = MustacheEngineBuilder
                 .newBuilder()
                 .addTemplateLocator(new ClassPathTemplateLocator(1, templatePathPrefix))
